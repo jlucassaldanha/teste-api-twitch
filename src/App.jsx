9789db32439d9c2dd63f79f526d6417450b91c97
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-06-23 13:05:29 -0300

    testes iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [params, setParams] = useState("Nada")
+  const api = axios.create({
+    baseURL: "https://api.twitch.tv/helix"
+  })
+
+  function getUsers() {
+    api.get("/users", )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <a onClick={() => setParams(document.location.hash)} href="https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=hatjqubn1mwj09m17p6tdfmj983tim&redirect_uri=http://localhost:5173&scope=user%3Aread%3Aemail+user%3Aread%3Afollows+moderator%3Aread%3Achatters+channel%3Aread%3Avips+moderation%3Aread" target='_blank'>Connect with Twitch</a>
+
+      <p>{ params.slice(14, params.indexOf("&scope")) }</p>
+    </div>
   )
 }
 
